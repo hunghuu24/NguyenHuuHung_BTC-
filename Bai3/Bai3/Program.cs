578d15da94c62d34809786f166fc4855f260652e
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-01-29 22:40:58 +0700

    Bài tập chuỗi

---
+++ Bai3/Bai3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Nhap chuoi: ");
+string s = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(s))
+{
+    Console.WriteLine("Chuoi KHONG doi xung");
+    return;
+}
+
+s = s.Replace(" ", "");
+
+int left = 0;
+int right = s.Length - 1;
+bool doiXung = true;
+
+while (left < right)
+{
+    if (s[left] != s[right])
+    {
+        doiXung = false;
+        break;
+    }
+    left++;
+    right--;
+}
+
+if (doiXung)
+    Console.WriteLine("Chuoi doi xung");
+else
+    Console.WriteLine("Chuoi khong doi xung");
