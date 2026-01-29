39bcf364fcea132ee2043bdf242044a4420aac26
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-01-29 08:14:23 +0700

    Bài tập chuỗi

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Nhap ho Ten: ");
+string hoTen = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(hoTen))
+    Console.WriteLine("Chuoi khong hop le");
+hoTen = hoTen.Trim();
+string[] word = hoTen.Split(' ');
+string kq = "";
+foreach (string w in word)
+{
+    if (w != "")
+    {
+        string tu = char.ToUpper(w[0]) + w.Substring(1).ToLower();
+        kq = kq + tu + " ";
+    }
+}
+Console.WriteLine(kq.Trim());
---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Nhap chuoi: ");
+String s = Console.ReadLine();
+if(string.IsNullOrWhiteSpace(s))
+{
+    Console.WriteLine("Khong co tu nao");
+}
+s = s.Trim();
+string[] words = s.Split(' ');
+int count = 0;
+foreach (string w in words)
+{
+    if (w != "")
+        count++;
+}
+Console.WriteLine("So tu: " + count);
